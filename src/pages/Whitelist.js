d48a39e0ac30f865606fcce7e03870773fe8c38c
SUBHAM KUNWAR <68282784+poisendev91@users.noreply.github.com>
2023-02-01 23:10:27 +0530

    Sdfg

--- src/pages/Whitelist.js
+++ src/pages/Whitelist.js
@@ -10,17 +10,19 @@ export const Whitelist = () => {
         <>
 
 
-            <section class="error" style={{ backgroundImage: `url(/assets/images/banner/congo.gif)`,  backgroundRepeat:" no-repeat;"}}>
+            <section class="error" style={{ backgroundImage: `url(/assets/images/banner/solana.gif)`, opacity: "0.8", backgroundRepeat:" no-repeat;"}}>
                 <div class="error__inner">
                     <div class="error__content" >
                         <div style={{ marginLeft: "auto", marginRight: "auto", width: "40%" }}>
                           
-                        <img class="d-block mb-4"  src="assets/images/banner/solana.gif" alt=" image" />
-                    </div>
-                        <h3 class=" mb-3" style={{ color:"black" }}>Hurrah! Your Wallet is Submitted and you are whitelisted.</h3>
-                       
-                        <h3 style={{ color: "black" }}>Join Discord to keep Updated</h3>
-                        <div class="text-center mt-5">
+                        <img class="d-block "  src="assets/images/banner/congo.gif" alt=" image" />
+                        </div>
+                        
+
+                        <div class="text-center " style={{ backgroundColor: "black", padding: "60px", borderRadius:"50px" }}>
+                            <h3 class=" mb-3" >Hurrah! Your Wallet is Submitted and you are whitelisted.</h3>
+
+                            <h3 style={{ marginBottom: "40px" }} >Join Discord to keep Updated</h3>
                             <a href="/" class="default-btn">Go Back To Home</a>
                             
                             <a href="https://discord.gg/vg7RYvGUSp" target="_blank" style={{marginLeft:"5px"} } class=" default-btn default-btn--secondary"><span><i class="fab fa-discord"></i> Join
