cbfdc62d6075a8bfb4fd08223bff986d74ddb832
SUBHAM KUNWAR <68282784+rabbit-sol@users.noreply.github.com>
2023-02-11 14:54:43 +0530

    Update Navbar.js

--- src/comp/Navbar.js
+++ src/comp/Navbar.js
@@ -36,7 +36,7 @@ export const Navbar = () => {
                                     </li>
 
                                     <li>
-                                        <a href="https://the-sushies.gitbook.io/thesushies/" target="_blank">WhitePaper</a>
+                                        <a href="https://thesushieswhitepaper.gitbook.io/thesushieswhitepaper/" target="_blank">WhitePaper</a>
                                     </li>
 
                                     <li>
